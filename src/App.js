66e3ffa317c31ef6a69c83ed640fb23258d28891
Elpuchu <carlospuchulan@gmail.com>
2021-08-26 21:43:34 -0500

    1 file modified

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ export default function App() {
   };
 
   async function requestDevice() {
-    console.log('imprimiendo...');
+    console.log('printing...');
     try {
-      let bluetoothDevice = await navigator.bluetooth.requestDevice({
+      /*const bluetoothDevice = await navigator.bluetooth.requestDevice({
         // filters: [ { name: 'Device test' } ],
         acceptAllDevices: true,
         optionalServices: [
@@ -23,6 +23,34 @@ export default function App() {
         'gattserverdisconnected',
         onDisconnected
       );
+
+      // Try to connect to the remote GATT Server running on the Bluetooth device
+      const server = await bluetoothDevice.gatt.connect();*/
+      await navigator.bluetooth
+        .requestDevice({
+          // filters: [ { name: 'Device test' } ],
+          acceptAllDevices: true,
+          optionalServices: [
+            'battery_service',
+            '03b80e5a-ede8-4b33-a751-6ce34ec4c700'
+          ]
+        })
+        .then(device => {
+          console.log('> Name:             ' + device.name);
+          console.log('> Id:               ' + device.id);
+          console.log('> Connected:        ' + device.gatt.connected);
+          alert(
+            'Name:' +
+              device.name +
+              ' - Id:' +
+              device.id +
+              ' - Connected:' +
+              device.gatt.connected
+          );
+        })
+        .catch(error => {
+          console.log('Argh! ' + error);
+        });
     } catch (error) {
       console.error(error);
       alert(error);
