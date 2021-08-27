d186e471b4bce46c2808ca8eeca49f54f3c4ebb8
Elpuchu <carlospuchulan@gmail.com>
2021-08-26 21:12:11 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-aptfns
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-aptfns)
---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './style.css';
+
+const onDisconnected = () => {
+  console.log('onDisconnected');
+};
+
+async function requestDevice() {
+  try {
+    let bluetoothDevice = await navigator.bluetooth.requestDevice({
+      // filters: [ { name: 'Device test' } ],
+      acceptAllDevices: true,
+      optionalServices: [
+        'battery_service',
+        '03b80e5a-ede8-4b33-a751-6ce34ec4c700'
+      ]
+    });
+
+    bluetoothDevice.addEventListener('gattserverdisconnected', onDisconnected);
+  } catch (error) {
+    console.error(error);
+    alert(error);
+  }
+}
+
+export default function App() {
+  const print = () => {
+    console.log('imprimiendo...');
+    requestDevice();
+  };
+
+  return (
+    <div>
+      <h1>Prueba lista de dispositivos para la Srta Milagros</h1>
+      <button onClick={print}>Ver dispositivo BT</button>
+    </div>
+  );
+}
