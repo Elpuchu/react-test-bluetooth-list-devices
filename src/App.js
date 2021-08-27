4b0243a57808bad3882b1faf1ba23c7defebc706
Elpuchu <carlospuchulan@gmail.com>
2021-08-26 21:30:09 -0500

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,38 +1,38 @@
 import React from 'react';
 import './style.css';
 
-const onDisconnected = () => {
-  console.log('onDisconnected');
-};
-
-async function requestDevice() {
-  try {
-    let bluetoothDevice = await navigator.bluetooth.requestDevice({
-      // filters: [ { name: 'Device test' } ],
-      acceptAllDevices: true,
-      optionalServices: [
-        'battery_service',
-        '03b80e5a-ede8-4b33-a751-6ce34ec4c700'
-      ]
-    });
-
-    bluetoothDevice.addEventListener('gattserverdisconnected', onDisconnected);
-  } catch (error) {
-    console.error(error);
-    alert(error);
-  }
-}
-
 export default function App() {
-  const print = () => {
-    console.log('imprimiendo...');
-    requestDevice();
+  const onDisconnected = event => {
+    alert(`The device ${event.target} is disconnected`);
+    setIsDisconnected(true);
   };
+
+  async function requestDevice() {
+    console.log('imprimiendo...');
+    try {
+      let bluetoothDevice = await navigator.bluetooth.requestDevice({
+        // filters: [ { name: 'Device test' } ],
+        acceptAllDevices: true,
+        optionalServices: [
+          'battery_service',
+          '03b80e5a-ede8-4b33-a751-6ce34ec4c700'
+        ]
+      });
+
+      bluetoothDevice.addEventListener(
+        'gattserverdisconnected',
+        onDisconnected
+      );
+    } catch (error) {
+      console.error(error);
+      alert(error);
+    }
+  }
 
   return (
     <div>
       <h1>Prueba lista de dispositivos para la Srta Milagros</h1>
-      <button onClick={print}>Holi!, ver dispositivos BT</button>
+      <button onClick={requestDevice}>Holi!, ver dispositivos BT</button>
     </div>
   );
 }
